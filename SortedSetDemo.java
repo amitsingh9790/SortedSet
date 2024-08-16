afa75b94cf644a72a966bd46f205af074a5afe81
Amit Singh <94777067+amitsingh9790@users.noreply.github.com>
2024-08-16 12:16:43 +0530

    Add files via upload

--- SortedSetDemo.java
+++ SortedSetDemo.java
@@ -1,10 +1,10 @@
 package SortedSet;
+import java.util.Comparator;
 import java.util.SortedSet;
 import java.util.TreeSet;
-
 public class SortedSetDemo {
     public static void main(String[] args) {
-        SortedSet set = new TreeSet();
+        SortedSet set = new TreeSet(Comparator.reverseOrder());
 
         set.add("aa");
         set.add("dd");
@@ -14,8 +14,10 @@ public class SortedSetDemo {
         set.add("cc");
 
         System.out.println(set);
-        System.out.println(set.headSet("cc"));
-        System.out.println(set.tailSet("cc"));
-        System.out.println(set.subSet("bb","ee"));
+        System.out.println("First: "+set.first()); // First elements
+        System.out.println("Last: "+set.last()); // Last elements
+        System.out.println("Previous than CC"+set.headSet("cc"));//Elements previous than cc
+        System.out.println("Next than or equal to CC"+set.tailSet("cc"));//Elements Next than or equal to cc
+        System.out.println(set.subSet("ee","bb")); //Elements strictly between bb(include) and ee(exclude)
     }
 }
